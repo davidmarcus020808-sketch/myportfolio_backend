72d1b14d7a33a70ba57b871ada1dea2c36e11b5f
David Marcus <davidmarcus020808@gmail.com>
2026-06-11 19:53:51 -0700

    Add legacy storage settings for cloudinary_storage compatibility

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -113,3 +113,16 @@ def project_detail(request, slug):
     project = get_object_or_404(Project.objects.prefetch_related("built"), slug=slug)
     serializer = ProjectSerializer(project, context={"request": request})
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+from django.conf import settings
+from django.http import JsonResponse
+
+def debug_storage(request):
+    return JsonResponse({
+        "default_storage_backend": settings.STORAGES.get("default", {}).get("BACKEND"),
+        "cloud_name_set": bool(settings.CLOUDINARY_STORAGE.get("CLOUD_NAME")),
+        "api_key_set": bool(settings.CLOUDINARY_STORAGE.get("API_KEY")),
+        "api_secret_set": bool(settings.CLOUDINARY_STORAGE.get("API_SECRET")),
+        "cloud_name_value": settings.CLOUDINARY_STORAGE.get("CLOUD_NAME"),
+    })
